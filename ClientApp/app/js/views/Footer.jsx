244b8768481a5efbeeb169bf654ff0d250b58854
Andrew <chenpoanandrew@gmail.com>
2015-05-04 18:34:15 +0800

    add link btn to github and my facebook

--- ClientApp/app/js/views/Footer.jsx
+++ ClientApp/app/js/views/Footer.jsx
@@ -18,7 +18,13 @@ var Footer = React.createClass({
       <footer className="footer">
         <span className="author">
             Author | Andrew Chen  {'( May / 2015 )'}
-			
+        </span>
+
+        <span className="author_link">
+            <a href="https://github.com/PolarBearAndrew/LabManager" >
+              <i className="fa fa-2x fa-github"></i> </a>
+            <a href="https://www.facebook.com/profile.php?id=100001317746154" >
+              <i className="fa fa-2x fa-facebook-official"></i> </a>
         </span>
       </footer>
     );
