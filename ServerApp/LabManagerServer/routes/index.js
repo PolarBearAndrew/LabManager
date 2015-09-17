da12ab6037e24197228299069102246b33931950
Andrew <chenpoanandrew@gmail.com>
2015-09-17 20:48:03 +0800

    modify link

--- ServerApp/LabManagerServer/routes/index.js
+++ ServerApp/LabManagerServer/routes/index.js
@@ -3,6 +3,10 @@ var router = require('express').Router();
 /*
  * homepage
  */
+router.get('/', function (req, res, next) {
+  res.render('index');
+});
+
 router.get('/LabManager.oit', function (req, res, next) {
 	res.render('index');
 });
